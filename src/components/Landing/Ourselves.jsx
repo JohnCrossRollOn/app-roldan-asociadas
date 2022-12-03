530555eddbc3534c9d06c75b336b381af45ae1ba
JohnCrossRollOn <108428761+JohnCrossRollOn@users.noreply.github.com>
2022-12-03 18:59:59 -0300

    3 files modified

--- src/components/Landing/Ourselves.jsx
+++ src/components/Landing/Ourselves.jsx
@@ -1,7 +1,12 @@
+import Hr from './Hr';
+
 export default () => {
   return (
     <>
-      <div className="grid grid-rows-6 gap-4 bg-white rounded-lg shadow-md p-4 w-[70vw] h-[50vh] overflow-hidden"></div>
+      <Hr>Nosotros</Hr>
+      <div className="grid grid-rows-6 gap-4 bg-white rounded-lg shadow-md p-4 w-full h-[60vh] overflow-hidden">
+        Aca hablariamos de nosotros
+      </div>
     </>
   );
 };
--- src/components/Landing/index.jsx
+++ src/components/Landing/index.jsx
@@ -1,11 +1,13 @@
 import Jumbotron from './Jumbotron';
 import Services from './Services';
+import Ourselves from './Ourselves';
 
 export default () => {
   return (
     <div className="grid grid-auto-cols gap-8 p-4 ">
       <Jumbotron />
       <Services />
+      <Ourselves />
     </div>
   );
 };
