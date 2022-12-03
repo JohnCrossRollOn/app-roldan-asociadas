302145056dcd686241aab0dbe2b308785a085d12
JohnCrossRollOn <108428761+JohnCrossRollOn@users.noreply.github.com>
2022-12-03 12:27:35 -0300

    1 binaries uploaded

--- src/components/Landing/Jumbotron.jsx
+++ src/components/Landing/Jumbotron.jsx
@@ -1,10 +1,10 @@
 export default () => {
   return (
     <div className="p-4 rounded-lg">
+      <img src="" />
       <span className="text-2xl font-bold">
         Nosotros somos tu estudio de abogacia!
       </span>
-      <img src="" />
     </div>
   );
 };
