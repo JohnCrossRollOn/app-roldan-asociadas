c87670a07bb5ddf6c1a9c8847b0e542bebe47571
JohnCrossRollOn <108428761+JohnCrossRollOn@users.noreply.github.com>
2022-12-03 13:36:15 -0300

    2 files modified

--- src/components/Landing/Services.jsx
+++ src/components/Landing/Services.jsx
@@ -1,11 +1,14 @@
-const Card = () => (
-  <div className="bg-slate-50 rounded-lg">Esto seria una card</div>
+const Card = ({ children }) => (
+  <div className="bg-white rounded-lg shadow-md p-2 w-fit">{children}</div>
 );
 
 export default () => {
   return (
-    <div>
-      <Card />
+    <div className="flex flex-col gap-4">
+      <p className="text-sm tracking-widest font-bold">SERVICIOS</p>
+      <Card>
+        <img src="" />
+      </Card>
     </div>
   );
 };
