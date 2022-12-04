f2bd1d964b9529589d6f48967df4032ebb1b4c4f
JohnCrossRollOn <108428761+JohnCrossRollOn@users.noreply.github.com>
2022-12-04 17:18:13 -0300

    1 file modified

--- src/components/Landing/Jumbotron.jsx
+++ src/components/Landing/Jumbotron.jsx
@@ -1,6 +1,6 @@
 export default () => {
   return (
-    <div className="w-full select-none rounded-lg flex flex-col gap-4 md:gap-8 md:grid md:grid-cols-2">
+    <div className="w-full select-none rounded-lg flex flex-col gap-4 md:gap-16 md:grid md:grid-cols-2">
       <img
         crossOrigin="anonymous"
         loading="lazy"
@@ -8,7 +8,7 @@ export default () => {
         alt="Nosotros con un juez"
         className=""
       />
-      <div>
+      <div className="flex flex-col gap-4">
         <p className="text-[2rem] leading-8 font-extrabold">
           Nosotros somos tu estudio de abogacia!
         </p>
