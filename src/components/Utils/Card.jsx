21fb2f09d01aeba9474ed2d003d55cb7583a218c
JohnCrossRollOn <108428761+JohnCrossRollOn@users.noreply.github.com>
2022-12-06 16:25:17 -0300

    2 files modified

--- src/components/Utils/Card.jsx
+++ src/components/Utils/Card.jsx
@@ -1,7 +1,7 @@
 export default (props) => {
   return (
     <div
-      className={`relative bg-white rounded-lg shadow-md p-4 w-[70vw] h-[50vh] h-full ${props.className}`}
+      className={`relative bg-white rounded-lg shadow-md p-4 w-[70vw] h-fit h-full ${props.className}`}
     >
       {props.children}
     </div>
