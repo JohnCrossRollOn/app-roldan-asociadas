0fe44ae0eab9c417a70cba603d66c86e616bceaf
JohnCrossRollOn <108428761+JohnCrossRollOn@users.noreply.github.com>
2022-12-06 16:19:48 -0300

    1 file modified

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -114,14 +114,14 @@ export default () => {
       <Hr>Contactenos</Hr>
       <div id="contacto" />
       <Slider>
-        <Card className="h-[80vh] md:h-[50vh] md:w-[30vw]">
+        <Card className="h-fit md:w-[30vw]">
           <ContactForm
             onSubmit={(e) => {
               console.log(e);
             }}
           />
         </Card>
-        <Card className="h-[80vh] md:h-[50vh] md:w-[30vw]">
+        <Card className="h-fit md:w-[30vw]">
           <div className="flex flex-col gap-8">
             <p className="text-2xl font-semibold">Horarios de Atencion</p>
             <div>
@@ -155,7 +155,7 @@ export default () => {
             </div>
           </div>
         </Card>
-        <Card className="h-[80vh] md:h-[50vh] md:w-[30vw]">
+        <Card className="h-fit md:w-[30vw]">
           <iframe
             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3289.101925606311!2d-58.66534508047211!3d-34.47493850492866!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95bca3690eb57bc3%3A0x2c340646770f9291!2sColombia%201076%2C%20B1618EMD%20El%20Talar%2C%20Provincia%20de%20Buenos%20Aires!5e0!3m2!1ses-419!2sar!4v1667701409545!5m2!1ses-419!2sar"
             className="w-full h-full rounded-lg"
