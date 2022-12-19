ba8001d3eb493b70437c4e4f2b2b38083e19837a
johncrossrollon <juanrolon54@outlook.com>
2022-12-19 17:01:47 +0000

    Update Ourselves.jsx and elements.js

--- src/components/Landing/Ourselves.jsx
+++ src/components/Landing/Ourselves.jsx
@@ -1,4 +1,4 @@
-import Hr from '../Utils/Hr';
+import Hr from "../Utils/Hr";
 
 export default () => {
   return (
@@ -7,26 +7,30 @@ export default () => {
       <Hr>Nosotros</Hr>
       <div id="nosotros" />
       <div className="flex flex-col gap-4">
-        <p className="text-2xl font-extrabold">Un equipo mas que calificado</p>
+        <p className="text-2xl font-extrabold">
+          Somos un equipo altamente calificado
+        </p>
         <div className=" bg-white rounded-lg p-2 italic shadow-md">
           <p className="drop-shadow-md text-primary font-semibold leading-tight tracking-wider">
-            <strong className="font-black">"</strong> Los pilares de nuestra
-            comunidad son algo que dijo Quique (Dr. Roldan Penayo) pero Gabriela
-            Fernanda Dominguez no le tomo fotografia.
+            <strong className="font-black">"</strong> Esto es una frase que nos
+            caracterize y represente.
             <strong className="font-black">"</strong>
           </p>
         </div>
         <p className="text-xs leading-relaxed">
-          Los pilares de nuestra comunidad son algo que dijo Quique (Dr. Roldan
-          Penayo) pero Gabriela Fernanda Dominguez no le tomo fotografia.
+          Esto es un texto mas descriptivo sobre quienes somos, que hacemos y
+          hacia donde vamos. Esto es un texto mas descriptivo sobre quienes
+          somos, que hacemos y hacia donde vamos.
         </p>
         <p className="text-xs leading-relaxed">
-          Los pilares de nuestra comunidad son algo que dijo Quique (Dr. Roldan
-          Penayo) pero Gabriela Fernanda Dominguez no le tomo fotografia.
+          Esto es un texto mas descriptivo sobre quienes somos, que hacemos y
+          hacia donde vamos. Esto es un texto mas descriptivo sobre quienes
+          somos, que hacemos y hacia donde vamos.
         </p>
         <p className="text-xs leading-relaxed">
-          Los pilares de nuestra comunidad son algo que dijo Quique (Dr. Roldan
-          Penayo) pero Gabriela Fernanda Dominguez no le tomo fotografia.
+          Esto es un texto mas descriptivo sobre quienes somos, que hacemos y
+          hacia donde vamos. Esto es un texto mas descriptivo sobre quienes
+          somos, que hacemos y hacia donde vamos.
         </p>
       </div>
     </>
